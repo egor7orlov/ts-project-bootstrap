d5e48211ccf08ef20091b5eb39d216e5422cf3f7
Yehor Orlov <egor7orlov@gmail.com>
2023-06-28 19:27:30 -0500

    Preparation script update. README added

---
+++ README.md
@@ -0,0 +1,22 @@
+# TypeScript project bootstrap
+
+---
+
+If you found yourself in situation where you've just executed `npm init` command but don't know what to do next with
+your TypeScript-powered project, or you haven't even done it yet just execute the next command:
+
+```shell
+curl -fsSL https://raw.githubusercontent.com/egor7orlov/ts-project-bootstrap/main/prepare_project.sh | sh
+```
+
+### What this script does?
+
+- Adds usable **TypeScript** configs (`tsconfig.json` and `tsconfig.build.json`)
+- Adds **ESLint** and **Prettier** configs with all messy rules described
+- Installs necessary dev-dependencies _(if you didn't execute `npm init` it will do it for you, so you can run this
+  script even in an empty folder)_
+
+### TODO:
+
+- Add automated **Jest** configuration
+- Add automated **Husky** configuration
--- prepare_project.sh
+++ prepare_project.sh
@@ -1,10 +1,43 @@
 #!/bin/sh
 
-configs_filenames="tsconfig.json tsconfig.build.json .eslintrc.js .prettierrc"
+# If `npm` is not installed suggest user to install Node.js via NVM
+if [ ! -$(command -v npm) ]; then
+  tput setaf 1 && tput bold && echo "==================================" && tput sgr0
+  echo
+  tput bold && echo "Please install Node.js first: " && tput sgr0
+  echo "https://github.com/nvm-sh/nvm#installing-and-updating"
+  echo
+  tput setaf 1 && tput bold && echo "==================================" && tput sgr0
+  exit 1
+fi
 
-for filename in $configs_filenames; do
-  curl -O "https://raw.githubusercontent.com/egor7orlov/ts-project-bootstrap/main/configs/$filename"
-done
+# If there is no package.json run `npm init`
+if [ ! -e "./package.json" ]; then
+  tput setaf 2 && tput bold && echo ">================================== Initial project configuration" && tput sgr0
+  echo
+
+  npm init
+
+  # If `npm init` was aborted we cancel execution too
+  if [ ! -e "./package.json" ]; then
+    exit 1
+  fi
+
+  # Create `src` folder with entry `index.ts` file
+  mkdir -p src
+  touch src/index.ts && echo "console.log('Hello world!')" >./src/index.ts
+  npm pkg set 'scripts.start'='tsc && node ./dist/index.js'
+
+  echo
+  tput setaf 2 && tput bold && echo "==================================> DONE" && tput reset
+  echo
+fi
+
+clear
+
+# Install necessary dev-dependencies
+tput setaf 2 && tput bold && echo ">================================== Dev-dependencies installation" && tput sgr0
+echo
 
 npm install -D @types/node \
   @types/node \
@@ -24,5 +57,34 @@ npm install -D @types/node \
   tsconfig-paths \
   typescript
 
-# TODO: add npm script for running jest
-# TODO: config husky
+echo
+tput setaf 2 && tput bold && echo "==================================> DONE" && tput sgr0
+echo
+
+clear
+
+tput setaf 2 && tput bold && echo ">================================== Fetching config files" && tput sgr0
+echo
+
+configs_filenames="tsconfig.json tsconfig.build.json .eslintrc.js .prettierrc"
+
+for filename in $configs_filenames; do
+  curl -O "https://raw.githubusercontent.com/egor7orlov/ts-project-bootstrap/main/configs/$filename"
+done
+
+echo
+tput setaf 2 && tput bold && echo "==================================> DONE" && tput sgr0
+echo
+
+clear
+
+# TODO: add `src` folder with index.ts file and add script `start` which compiles and runs this file
+# TODO: add Jest configuration an npm script for running tests
+# TODO: configure Husky
+
+tput setaf 2 && tput bold && echo "==================================" && tput sgr0
+echo
+tput bold && echo "You are all set up! Try running \`npm run start\`" && tput sgr0
+echo
+tput setaf 2 && tput bold && echo "==================================" && tput sgr0
+exit 1
